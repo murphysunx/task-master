81e5c0565718109611742ea5d0ca3d078381de16
Murphy <murphysunx@gmail.com>
2024-07-24 21:28:03 +1000

    feat: Implement delete, getByUserId, and update endpoints in TaskController

--- src/task/task.controller.spec.ts
+++ src/task/task.controller.spec.ts
@@ -1,8 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
 import { UserNotFound } from 'src/user/exceptions/user-not-found/user-not-found';
-import { Task } from './entity/task';
 import { CreateTaskDto } from './dto/create-task/create-task';
+import { Task } from './entity/task';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
 
@@ -61,5 +61,66 @@ describe('TaskController', () => {
     await expect(controller.create(createTaskDto)).rejects.toThrow(
       UserNotFound,
     );
+  });
+
+  it('should delete task by taskId', async () => {
+    const taskId = '1';
+    const expectedTask: Task = new Task({
+      id: BigInt(1),
+      title: 'Task 1',
+      userId: BigInt(1),
+      completed: false,
+      description: 'Description 1',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+    taskService.deleteTaskById.mockResolvedValue(expectedTask);
+    const task = await controller.delete(taskId);
+    expect(taskService.deleteTaskById).toHaveBeenCalledWith(taskId);
+    expect(task).toEqual(expectedTask);
+  });
+
+  it('should get all tasks by userId', async () => {
+    const userId = '1';
+    const expectedTasks: Task[] = [
+      new Task({
+        id: BigInt(1),
+        title: 'Task 1',
+        userId: BigInt(1),
+        completed: false,
+        description: 'Description 1',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }),
+    ];
+    taskService.getAllTasksByUserId.mockResolvedValue(expectedTasks);
+    const tasks = await controller.getByUserId(userId);
+    expect(taskService.getAllTasksByUserId).toHaveBeenCalledWith(userId);
+    expect(tasks).toEqual(expectedTasks);
+  });
+
+  it('should update task by taskId', async () => {
+    const taskId = '1';
+    const updateTaskDto = {
+      title: 'Task 1',
+      description: 'Description 1',
+      completed: true,
+    };
+    const expectedTask: Task = new Task({
+      id: BigInt(1),
+      title: updateTaskDto.title,
+      userId: BigInt(1),
+      completed: updateTaskDto.completed,
+      description: updateTaskDto.description,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+    taskService.updateTaskById.mockResolvedValue(expectedTask);
+    const task = await controller.update(taskId, updateTaskDto);
+    expect(taskService.updateTaskById).toHaveBeenCalledWith(
+      taskId,
+      updateTaskDto,
+    );
+    expect(task).toEqual(expectedTask);
   });
 });
--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task/create-task';
+import { UpdateTaskDto } from './dto/update-task/update-task';
 import { Task } from './entity/task';
 import { TaskService } from './task.service';
 
@@ -10,5 +20,23 @@ export class TaskController {
   @Post()
   async create(createTaskDto: CreateTaskDto): Promise<Task> {
     return this.taskService.createTask(createTaskDto);
+  }
+
+  @Delete(':taskId')
+  async delete(@Param('taskId') taskId: string): Promise<Task> {
+    return this.taskService.deleteTaskById(taskId);
+  }
+
+  @Get()
+  async getByUserId(@Query('userId') userId: string): Promise<Task[]> {
+    return this.taskService.getAllTasksByUserId(userId);
+  }
+
+  @Patch(':taskId')
+  async update(
+    @Param('taskId') taskId: string,
+    @Body() updateTaskDto: UpdateTaskDto,
+  ): Promise<Task> {
+    return this.taskService.updateTaskById(taskId, updateTaskDto);
   }
 }

9a0e1446b2f6286ccb8273bd1e893367b370b4bf
Murphy <murphysunx@gmail.com>
2024-08-04 20:39:08 +1000

    feat: task list model

---
+++ prisma/migrations/20240804103846_task_list/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `taskList` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `userId` BIGINT NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `taskList` ADD CONSTRAINT `taskList_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
